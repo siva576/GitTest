9794028e93f28e0bd18160c79af3ee50523b6d84
sivad <siva.s@blueoceanmi.com>
2018-11-19 17:23:47 +0530

    Components Part 2

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -10,6 +10,10 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 
 import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
 
 
 @NgModule({
@@ -22,7 +26,10 @@ import { MatListModule } from '@angular/material/list';
     BrowserAnimationsModule,
     MatToolbarModule,
     FlexLayoutModule,
-    MatListModule
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

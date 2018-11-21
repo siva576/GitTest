c8b51c7e885190f47c6ce4b4a785cd1f30915d0a
sivad <siva.s@blueoceanmi.com>
2018-11-21 18:02:22 +0530

    Reactive Forms Part 1

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -34,6 +34,10 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { FormsModule } from '@angular/forms';
 
 
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -61,7 +65,10 @@ import { FormsModule } from '@angular/forms';
     MatFormFieldModule,
     MatInputModule,
     MatCheckboxModule,
-    FormsModule
+    FormsModule,
+    MatSelectModule,
+    MatSlideToggleModule,
+    ReactiveFormsModule
   ],
   providers: [DishService, PromotionService, LeaderService],
   bootstrap: [AppComponent],
